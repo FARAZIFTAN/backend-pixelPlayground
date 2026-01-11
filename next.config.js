31dbeed8c0ce26e7f33972063608fda55e92b8e8
jif <jiftan3@gmail.com>
2026-01-12 01:53:21 +0700

    hapus procces .env

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
-const FRONTEND_URL = process.env.FRONTEND_URL || 'https://karyaklik.netlify.app';
+
+// HAPUS process.env, KITA HARDCODE LANGSUNG AGAR TIDAK ADA ALASAN LAGI
+const FRONTEND_URL = 'https://karyaklik.netlify.app'; 
 
 const nextConfig = {
-  // Add global headers for API routes to allow cross-origin requests
   async headers() {
     return [
       {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: FRONTEND_URL },
+          { key: 'Access-Control-Allow-Origin', value: FRONTEND_URL }, // Sekarang ini pasti nilainya Netlify
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
         ],
